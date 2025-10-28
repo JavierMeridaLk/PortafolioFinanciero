09f2e82e1e9b165dbd4f58ce6d96c79dc35df7ef
Javier Mérida <javiermerida202230638@cunoc.edu.gt>
2025-10-28 03:01:14 -0600

    Add pages

--- portafolio/src/App.vue
+++ portafolio/src/App.vue
@@ -1,30 +1,108 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<template>
+  <main class=" bg-black">
+    <aside
+      class="fixed top-0 left-0 w-80 h-full backdrop-blur bg-zinc-950/85 overflow-y-auto z-40 transition-transform duration-300"
+      :class="{
+        'translate-x-0': menuOpen,
+        '-translate-x-full': !menuOpen,
+      }"
+    >
+      <div class="px-6 pt-5 pb-6 sticky top-0  !bg-zinc-950 ">
+        <div class="flex items-center justify-between">
+          <AnOutlinedStock class="size-12 text-indigo-600" />
+          <button
+            class="inline-block md:hidden items-center cursor-pointer p-2.5 my-2 hover:bg-white/10 transition-colors rounded-lg"
+            @click="menuOpen = !menuOpen"
+          >
+            <ClCloseMd />
+          </button>
+        </div>
+        <h1 class="text-2xl font-semibold">Inversiones</h1>
+      </div>
+      <nav class="py-4">
+        <ul class="space-y-1.5">
+          <ListItem v-for="item in menuItems" :key="item.name" :item="item" />
+        </ul>
+      </nav>
+    </aside>
+    <div class="ml-0 md:ml-80 h-screen overflow-auto">
+      <label
+        class="inline-block md:hidden items-center cursor-pointer p-2.5 mx-4 mt-6 hover:bg-white/10 transition-colors rounded-lg"
+      >
+        <input type="checkbox" class="hidden" v-model="menuOpen" />
+        <ChMenuHamburger />
+      </label>
+      <router-view v-slot="{ Component, route }">
+        <transition name="fade-up" mode="out-in">
+          <component :is="Component" :key="route.path" />
+        </transition>
+      </router-view>
+      <!-- <RouterView /> -->
+    </div>
+  </main>
+</template>
+<script setup lang="jsx">
+  import {
+    AnOutlinedStock,
+    ChMenuHamburger,
+    ClCloseMd,
+  } from "@kalimahapps/vue-icons";
+  import { breakpointsTailwind, useBreakpoints } from "@vueuse/core";
+  import { defineComponent, ref } from "vue";
+  import { RouterLink } from "vue-router";
+  import { menuItems } from "./data/indexData";
+
+  const breakpoint = useBreakpoints(breakpointsTailwind);
+  const menuOpen = ref(breakpoint.smaller("sm").value ? false : true);
+
+  const ListItem = defineComponent({
+    props: {
+      item: Object,
+    },
+    setup(props) {
+      const Icon = props.item.icon;
+      return () => (
+        <>
+          {props.item.separator ? (
+            <li class="py-2 pl-6 text-xs font-light uppercase tracking-widest text-gray-300">
+              {props.item.name}
+            </li>
+          ) : (
+            <li>
+              <RouterLink
+                to={props.item.path}
+                onClick={() => {
+                  breakpoint.smaller("sm").value && (menuOpen.value = false);
+                }}
+                active-class={`bg-black font-bold hover:!bg-black px-3 ${props.item.class}`}
+                class="py-4 flex items-center gap-3 hover:bg-black  rounded-tl-xl rounded-bl-xl pl-4 ml-4 transition-all"
+              >
+                <Icon class="w-6 h-6" />
+                {props.item.name}
+              </RouterLink>
+            </li>
+          )}
+        </>
+      );
+    },
+  });
 </script>
 
-<template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
-
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+  :root {
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI",
+      Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
+      sans-serif;
+  }
+
+  .fade-up-enter-active,
+  .fade-up-leave-active {
+    transition: opacity 0.3s ease-out, transform 0.3s ease-out;
+  }
+
+  .fade-up-enter-from,
+  .fade-up-leave-to {
+    opacity: 0;
+    transform: translateY(35px);
+  }
 </style>
---
+++ portafolio/src/components/Team.vue
@@ -0,0 +1,64 @@
+<template>
+    <div>
+        <header
+        class="sticky top-0 px-8 md:py-12 md:px-20 z-10 bg-black/30 backdrop-blur-xl text-2xl font-semibold py-6"
+        >
+        <h1>Equipo de Trabajo</h1>
+        <h2 class="text-xl font-semibold mt-4">Conformado por:</h2>
+        </header>
+        <div
+        class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4 md:px-20 md:py-12 px-8 pt-2 pb-20"
+        >
+        <div
+            v-for="member in team"
+            :key="member.ra"
+            class="bg-slate-950 rounded-xl shadow-md p-4 flex flex-col items-center gap-4"
+        >
+            <img
+            :src="member.img"
+            alt="Foto de perfil"
+            class="size-48 object-cover rounded-xl aspect-square"
+            />
+            <div class="text-center">
+            <h3 class="text-lg font-semibold">{{ member.name }}</h3>
+            <p class="text-sm text-gray-500">{{ member.ra }}</p>
+            </div>
+        </div>
+        </div>
+    </div>
+</template>
+<script setup>
+    const team = [
+        {
+        name: "Javier Alejandro Mérida Gómez",
+        ra: "202230638",
+        img: "/img/xavi.jpg",
+        },
+        {
+        name: "Fredy",
+        ra: "202030851",
+        img: "/img/fredy.jpg",
+        },
+        {
+        name: "Kike",
+        ra: "202231051",
+        img: "/img/kike.jpg",
+        },
+        {
+        name: "Aleko",
+        ra: "201930761",
+        img: "/img/alejo.jpg",
+        },
+        {
+        name: "Monse",
+        ra: "202230884",
+        img: "/img/monse.jpg",
+        },
+        {
+        name: "Fer",
+        ra: "202230884",
+        img: "/img/fer.jpg",
+        },
+    ];
+</script>
+<style scoped></style>
