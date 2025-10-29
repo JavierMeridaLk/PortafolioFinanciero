ea3453f4844f30e449fa8c60370ba33f3d626410
Monserrat Gomez <mariagomez202030849@cunoc.edu.gt>
2025-10-29 11:35:44 -0600

    Update Index.vue

--- portafolio/src/components/Index.vue
+++ portafolio/src/components/Index.vue
@@ -2,71 +2,58 @@
     <div class="min-h-screen bg-gradient-to-br from-slate-950 via-black to-slate-900 text-white">
         <!-- Encabezado -->
         <header
-        class="sticky top-0 z-10 px-8 md:px-20 py-8 md:py-12 bg-gradient-to-r from-slate-950/80 to-black/70 backdrop-blur-xl border-b border-white/10 shadow-lg"
-        >
-        <h1 class="mb-2 text-4xl font-bold tracking-tight">Vista General</h1>
-        <h2 class="text-lg font-medium text-gray-400">
-            Resumen de Inversiones
-        </h2>
+            class="sticky top-0 z-10 px-8 md:px-20 py-8 md:py-12 bg-gradient-to-r from-slate-950/80 to-black/70 backdrop-blur-xl border-b border-white/10 shadow-lg">
+            <h1 class="mb-2 text-4xl font-bold tracking-tight">Vista General</h1>
+            <h2 class="text-lg font-medium text-gray-400">
+                Resumen de Inversiones
+            </h2>
         </header>
 
         <!-- Sección principal -->
         <section class="md:px-20 px-8 pb-20 pt-10">
-        <!-- Tarjetas -->
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-            <div
-            v-for="item in indexData.topCards"
-            :key="item.title"
-            class="group bg-gradient-to-br from-slate-900/80 to-slate-800/50 border border-white/10 rounded-2xl p-6 shadow-lg hover:shadow-indigo-800/30 transition-all duration-500 hover:scale-[1.02]"
-            >
-            <div class="flex flex-col gap-y-3">
-                <component
-                :is="item.icon"
-                class="size-8 text-indigo-300 group-hover:text-indigo-400 transition-colors duration-300"
-                />
-                <h6
-                class="text-xl font-semibold tracking-wide"
-                :class="item.class"
-                >
-                {{ item.title }}
-                </h6>
-                <code
-                :class="{
-                    'font-black !text-2xl': item.title === 'Rendimiento Total',
-                    'text-red-400':
-                    item.value < 0 && item.title === 'Rendimiento Total',
-                    'text-green-400':
-                    item.value >= 0 && item.title === 'Rendimiento Total',
-                }"
-                class="text-xl font-semibold"
-                >
+            <!-- Tarjetas -->
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+                <div v-for="item in indexData.topCards" :key="item.title"
+                    class="group bg-gradient-to-br from-slate-900/80 to-slate-800/50 border border-white/10 rounded-2xl p-6 shadow-lg hover:shadow-indigo-800/30 transition-all duration-500 hover:scale-[1.02]">
+                    <div class="flex flex-col gap-y-3">
+                        <component :is="item.icon"
+                            class="size-8 text-indigo-300 group-hover:text-indigo-400 transition-colors duration-300" />
+                        <h6 class="text-xl font-semibold tracking-wide" :class="item.class">
+                            {{ item.title }}
+                        </h6>
+                        <code :class="{
+                            'font-black !text-2xl': item.title === 'Rendimiento Total',
+                            'text-red-400':
+                                item.value < 0 && item.title === 'Rendimiento Total',
+                            'text-green-400':
+                                item.value >= 0 && item.title === 'Rendimiento Total',
+                        }" class="text-xl font-semibold">
                 {{
                     item.value.toLocaleString("en-GB", {
-                    style: "currency",
-                    currency: "USD",
-                    signDisplay:
-                        item.title === "Rendimiento Total"
-                        ? "exceptZero"
-                        : "auto",
+                        style: "currency",
+                        currency: "USD",
+                        signDisplay:
+                            item.title === "Rendimiento Total"
+                                ? "exceptZero"
+                                : "auto",
                     })
                 }}
                 </code>
-            </div>
-            <h1 class="text-2xl font-semibold py-6">Flujo de las ganancias</h1>
-            <div class="">
-                <div ref="chartDiv" class="rounded-lg overflow-hidden ring-2 ring-white/15"></div>
-            </div>
-        </div>
+                    </div>
+                    <h1 class="text-2xl font-semibold py-6">Flujo de las ganancias</h1>
+                    <div class="">
+                        <div ref="chartDiv" class="rounded-lg overflow-hidden ring-2 ring-white/15"></div>
+                    </div>
+                </div>
 
-        <!-- Gráfico -->
-        <h1 class="text-2xl font-semibold pt-16 pb-6 text-gray-200">
-            Flujo general de inversión
-        </h1>
+                <!-- Gráfico -->
+                <h1 class="text-2xl font-semibold pt-16 pb-6 text-gray-200">
+                    Flujo general de inversión
+                </h1>
 
-        <div
-            ref="chartDiv"
-            class="rounded-2xl overflow-hidden ring-1 ring-white/10 shadow-2xl shadow-indigo-900/30"
-        ></div>
+                <div ref="chartDiv"
+                    class="rounded-2xl overflow-hidden ring-1 ring-white/10 shadow-2xl shadow-indigo-900/30"></div>
+                    </div>
         </section>
     </div>
 </template>
@@ -112,7 +99,7 @@ onMounted(() => {
         bottomFillColor2: "rgba(122, 46, 199,0.6)",
     });
 
-    
+
 
     areaSeries.setData([
         { value: 5.94, time: 1751500800 }, // 4/07/2025
